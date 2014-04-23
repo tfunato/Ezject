2b2da6cddfd07c5882cd97ca216a8668de509873
tfunato <tfunato@gmail.com>
2014-04-24 00:50:20 +0900

    Mount、UnMount Implement

--- ezject/AppDelegate.m
+++ ezject/AppDelegate.m
@@ -13,12 +13,14 @@
 
     NSStatusItem *statusItem;
 
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     [self setupStatusItem];
     
     // Setup system event notifications
     [self fileNotifications];
+
 }
 
 - (void)setupStatusItem
@@ -48,11 +50,46 @@
 - (void) receiveSleepNote: (NSNotification*) note
 {
     NSLog(@"\n I like sleep.");
+
+    NSTask *task  = [[NSTask alloc] init];
+    NSPipe *pipe  = [[NSPipe alloc] init];
+    [task setLaunchPath: @"/usr/sbin/diskutil"];
+    [task setArguments: [NSArray arrayWithObjects: @"unmountDisk", @"disk1", nil]];
+    [task setStandardOutput: pipe];
+    [task launch];
+    
+    [task waitUntilExit];
+    
+    NSData* data = [[[task standardOutput] fileHandleForReading] readDataToEndOfFile];
+    NSString *result = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSLog(@"%@", result);
+
 }
 
 - (void) receiveWakeNote: (NSNotification*) note
 {
     NSLog(@"\n I like awake.");
+    
+    NSUserNotification *notification = [[NSUserNotification alloc] init];
+    notification.title = @"Awake!";
+    notification.informativeText = @"Awake is now.";
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+
+
+    NSTask *task  = [[NSTask alloc] init];
+    NSPipe *pipe  = [[NSPipe alloc] init];
+    [task setLaunchPath: @"/usr/sbin/diskutil"];
+    [task setArguments: [NSArray arrayWithObjects: @"mountDisk", @"disk1", nil]];
+    [task setStandardOutput: pipe];
+    [task launch];
+    
+    [task waitUntilExit];
+    
+    NSData* data = [[[task standardOutput] fileHandleForReading] readDataToEndOfFile];
+    NSString *result = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSLog(@"%@", result);
+    
+    //NSLog(@"\n I like awake.");
 }
 @end
 
